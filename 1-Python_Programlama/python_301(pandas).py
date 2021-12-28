e4378972dda54849d5c9cd4f1cc901bf851898e7
0nurakdeniz <akdeniz.o@hotmail.com>
2021-12-28 17:11:39 +0300

    ,

--- 1-Python_Programlama/python_301(pandas).py
+++ 1-Python_Programlama/python_301(pandas).py
@@ -19,9 +19,22 @@ tail = dataFrame1.tail()
 
 #pandas basic method
 
-print(dataFrame1.columns)
-print(dataFrame1.info())
+#print(dataFrame1.columns)
+#print(dataFrame1.info())
 
-print(dataFrame1.dtypes)
+#print(dataFrame1.dtypes)
 
-print('\n\n',dataFrame1.describe())
+#print('\n\n',dataFrame1.describe()) 
+
+#------indexing and slicing
+
+print(dataFrame1["NAME"])
+print(dataFrame1.AGE)
+
+dataFrame1["yeni_feature"] = [-1,-2,-3,-4,-5,-6]
+print(dataFrame1.yeni_feature)
+print('////////////')
+
+print(dataFrame1.loc[:3, "NAME":"AGE"])
+
+print(dataFrame1.loc[::-1,:])
