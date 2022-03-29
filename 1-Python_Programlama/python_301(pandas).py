e5e349f4c4051f7cf4d741abc763ef7eb4bea174
0nurakdeniz <akdeniz.o@hotmail.com>
2022-03-29 17:33:15 +0300

    Revert "for pandas"
    This reverts commit 36203d82f4a5cf3db1a3b33120999570949748d7.

--- 1-Python_Programlama/python_301(pandas).py
+++ 1-Python_Programlama/python_301(pandas).py
@@ -28,65 +28,13 @@ tail = dataFrame1.tail()
 
 #------indexing and slicing
 
-#print(dataFrame1["NAME"])
-#print(dataFrame1.AGE)
+print(dataFrame1["NAME"])
+print(dataFrame1.AGE)
 
 dataFrame1["yeni_feature"] = [-1,-2,-3,-4,-5,-6]
 print(dataFrame1.yeni_feature)
 print('////////////')
 
-#print(dataFrame1.loc[:3, "NAME":"AGE"])
+print(dataFrame1.loc[:3, "NAME":"AGE"])
 
-#print(dataFrame1.loc[::-1,:])
-
-
-# -- filtering
-
-filtre1 = dataFrame1.MAAS > 200
-
-#print (type(filtre1))
-
-filtelenmis = dataFrame1[filtre1]
-#print(filtelenmis)
-print('---------------------------')
-print(dataFrame1[dataFrame1.AGE >= 26])
-
-#-- List Comprehension
-
-print(dataFrame1.MAAS.mean())
-
-ort_maas = dataFrame1.MAAS.mean()
-
-import numpy as np
-
-print(np.mean(dataFrame1.MAAS))
-
-dataFrame1["maas_sevyesi"] = ["yüksek" if ort_maas < x else "dusuk" for x in dataFrame1.MAAS]
-print(dataFrame1)
-
-dataFrame1.columns = [each.lower() for each in dataFrame1.columns]
-print(dataFrame1)
-
-#-- concatenating data
-dataFrame1.drop(["yeni_feature"],axis=1,inplace=True)
-print (dataFrame1)
-
-data1= dataFrame1.head()
-data2= dataFrame1.tail()
-
-print ('******************')
-
-#vertical
-print( pd.concat([data1,data2],axis=0))
-
-#horizantal
-
-print (pd.concat([dataFrame1.maas,dataFrame1.age],axis=1))
-
-# -- transforming 
-
-def multiply(age):
-    return age*2
-
-dataFrame1["apply_metodu"] = dataFrame1.age.apply(multiply)
-print(dataFrame1)
+print(dataFrame1.loc[::-1,:])
