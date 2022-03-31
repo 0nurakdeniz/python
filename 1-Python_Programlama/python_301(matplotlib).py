c435982854ade09a6b320b91384c34d450cc66c3
0nurakdeniz <akdeniz.o@hotmail.com>
2022-03-31 11:48:42 +0300

    for matplot

--- 1-Python_Programlama/python_301(matplotlib).py
+++ 1-Python_Programlama/python_301(matplotlib).py
@@ -1,20 +1,41 @@
+
 import pandas as pd
 
 df = pd.read_csv("iris.csv")
 
-print(df.columns)
+#print(df.columns)
 
-print (df.Species.unique())
+#print (df.Species.unique())
 
-print(df.info())
+#print(df.info())
 
-print(df.describe())
+#print(df.describe())
 
 setosa = df[df.Species =='Iris-setosa']
 versicolor = df[df.Species == 'Iris-versicolor']
+virginica = df[df.Species == 'Iris-virginica']
 
-print('setosa        :')
-print(setosa.describe())
-print('versicolor    :')
-print(versicolor.describe())
 
+#print('setosa        :')
+#print(setosa.describe())
+#print('versicolor    :')
+#print(versicolor.describe())
+#print(virginica.describe())
+
+import matplotlib.pyplot as plt
+#line plot
+df1= df.drop(["Id"],axis=1)
+
+df1.plot(grid=True,linestyle=":")
+plt.show()
+
+plt.plot(setosa.Id,setosa.PetalLengthCm , color = "red", label = "setosa")
+plt.plot(versicolor.Id,versicolor.PetalLengthCm , color = "green", label = "versicolor")
+plt.plot(virginica.Id,virginica.PetalLengthCm , color = "yellow", label = "virginica")
+
+plt.xlabel("Id")
+plt.ylabel("PetalLengthCm")
+plt.legend()
+plt.show()
+
+#scatter plot
